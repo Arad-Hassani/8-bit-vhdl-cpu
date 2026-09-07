c11fa98ed8f9e2300befd4470c227b270cf99130
Arad-Hassani <arad.hassani7@gmail.com>
2026-09-07 12:52:16 -0400

    Update reg_unit.vhd

--- reg_unit.vhd
+++ reg_unit.vhd
@@ -2,24 +2,31 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 ENTITY reg_unit IS
-  PORT (
-    A   : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);  -- 8-bit input
-    clk : IN  STD_LOGIC;                     -- clock
-    res : IN  STD_LOGIC;                     -- synchronous reset (active low)
-    Q   : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)   -- 8-bit output
-  );
+    PORT (
+        A   : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);  -- 8-bit data input
+        clk : IN  STD_LOGIC;                     -- clock
+        res : IN  STD_LOGIC;                     -- active-low asynchronous reset
+        Q   : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)   -- registered 8-bit output
+    );
 END reg_unit;
+
 
 ARCHITECTURE behavior OF reg_unit IS
 BEGIN
-  PROCESS(clk)
-  BEGIN
-    IF rising_edge(clk) THEN
-      IF res = '0' THEN
-        Q <= (OTHERS => '0');
-      ELSE
-        Q <= A;
-      END IF;
-    END IF;
-  END PROCESS;
+
+    PROCESS(clk, res)
+    BEGIN
+
+        -- Active-low asynchronous reset
+        IF res = '0' THEN
+            Q <= (OTHERS => '0');
+
+        -- Capture input on rising clock edge
+        ELSIF rising_edge(clk) THEN
+            Q <= A;
+
+        END IF;
+
+    END PROCESS;
+
 END behavior;
